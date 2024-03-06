ddedb2ee6400e112be000beb150fa66fafd2b1f7
Antoniomorales17 <126864866+Antoniomorales17@users.noreply.github.com>
2024-03-06 11:13:09 +0100

    Runable

---
+++ 2. Java Avanzado/Ejercicios semana 1/Java Avanzado Clase 2 - Día 3/Ejercicio Nº 1 - Simulación de un centro de atención al cliente/Ejercicio N 1  Simulacion de un centro de atencion al cliente/src/Cliente.java
@@ -0,0 +1,15 @@
+public class Agente {
+    private String nombre;
+
+    public Agente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
